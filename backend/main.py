748ede3eb0343e7ff705d700ae51f948e3641d7f
qzart45hi <qwert45hi@yandex.ru>
2022-04-16 15:29:32 +0300

    feat: backend socketio event listeners & emitter.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
+from flask import request
+
 from app import app
 from flask_socketio import SocketIO
 
 socketio = SocketIO(app)
+
+
+@socketio.on("connect")
+def socket_connect(auth=None):
+    print("connected:", auth, request.sid)
+
+
+@socketio.on("disconnect")
+def socket_disconnect():
+    print("disconnected:", request.sid)
 
 
 if __name__ == "__main__":
