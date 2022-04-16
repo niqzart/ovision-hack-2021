769ff01fdc58a6e806b312fe7d983f618a3a1b8e
qzart45hi <qwert45hi@yandex.ru>
2022-04-16 20:06:28 +0300

    feat: ssl sertificates & routing

--- backend/start.sh
+++ backend/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gunicorn --bind 0.0.0.0:5000 --worker-class eventlet -w 1 main:app
+gunicorn --certfile=/etc/nginx/ssl/live/faceprints.hopto.org/fullchain.pem --keyfile=/etc/nginx/ssl/live/faceprints.hopto.org/privkey.pem --bind 0.0.0.0:5000 --worker-class eventlet -w 1 main:app
--- frontend/src/utils/effects.js
+++ frontend/src/utils/effects.js
@@ -6,7 +6,7 @@ export function useSocketIO() {
   const [socket, setSocket] = useState(null)
 
   useEffect(() => {
-    const socket = io("http://localhost:5000")
+    const socket = io(`https://${window.location.hostname}:5000`)
     socket.on("connect", () => console.log("sio connected", socket.id))
     socket.on("disconnect", () => console.log("sio disconnected", socket.id))
     setSocket(socket)
