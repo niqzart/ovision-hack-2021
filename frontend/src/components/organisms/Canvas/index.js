314d8a44fa67b0fc7cb07c76dd040287ed507d2d
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-04-16 22:53:56 +0300

    add: center canvas, styling

--- frontend/src/components/organisms/Canvas/index.js
+++ frontend/src/components/organisms/Canvas/index.js
@@ -5,31 +5,41 @@ import {getFaceCoordinates} from "./face";
 import "./index.scss"
 
 
-async function getTransformedImageFromVideo(image_container, video, width, height) {
+function scaleChildFitParentMaxWidthOrHeight(child) {
+    const parent = child.parentElement;
+    const scaleX = parent.clientWidth / child.width;
+    const scaleY = parent.clientHeight / child.height;
+    const scalePercent = scaleX < scaleY ? scaleX : scaleY;
+    if (scalePercent === 1.) return;
+    child.width *= scalePercent;
+    child.height *= scalePercent;
+}
+
+
+async function getTransformedImageFromVideo(canvas_container, video, width, height) {
     let faceDimensions = await getFaceCoordinates(video);
-    for (let i = faceDimensions.length; i < image_container.childElementCount; ++i) {
-        image_container.removeChild(image_container.children[i]);
+    for (let i = faceDimensions.length; i < canvas_container.childElementCount; ++i) {
+        canvas_container.removeChild(canvas_container.children[i]);
     }
-    for (let i = image_container.childElementCount; i < faceDimensions.length; ++i) {
-        image_container.appendChild(document.createElement("canvas"));
+    for (let i = canvas_container.childElementCount; i < faceDimensions.length; ++i) {
+        canvas_container.appendChild(document.createElement("canvas"));
     }
     for (let i = 0; i < faceDimensions.length; ++i) {
         const iFace = faceDimensions[i];
-        image_container.children[i].setAttribute("width", width);
-        image_container.children[i].setAttribute("height", height);
-        let context = image_container.children[i].getContext("2d");
+        canvas_container.children[i].setAttribute("width", width);
+        canvas_container.children[i].setAttribute("height", height);
+        let context = canvas_container.children[i].getContext("2d");
         context.drawImage(video, iFace.topLeftX, iFace.topLeftY, iFace.width, iFace.height, 0, 0, width, height);
     }
 }
 
-async function drawRectangleOnImage(image, video, lineWidth, strokeStyle) {
+async function drawRectangleOnImage(canvas, video, lineWidth, strokeStyle) {
     const faceDimensions = await getFaceCoordinates(video);
-    const vhs = image.height / video.videoHeight;
-    const vws = image.width / video.videoWidth;
-    const context = image.getContext("2d");
-    image.width = image.clientWidth;
-    image.height = image.clientHeight;
-    context.drawImage(video, 0, 0, image.width, image.height);
+    const vhs = canvas.height / video.videoHeight;
+    const vws = canvas.width / video.videoWidth;
+    const context = canvas.getContext("2d");
+    scaleChildFitParentMaxWidthOrHeight(canvas);
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
     for (let i = 0; i < faceDimensions.length; ++i) {
         const iFace = faceDimensions[i];
         context.beginPath();
@@ -39,6 +49,7 @@ async function drawRectangleOnImage(image, video, lineWidth, strokeStyle) {
         context.stroke();
     }
 }
+
 
 
 class Canvas extends React.Component {
@@ -60,6 +71,8 @@ class Canvas extends React.Component {
     }
 
     handleRepeatCanvasDisplay = () => {
+        this.canvas.current.width = this.video.current.videoWidth;
+        this.canvas.current.height = this.video.current.videoHeight;
         this.image.timeout = setInterval(() => {
             drawRectangleOnImage(this.canvas.current, this.video.current, "4", "green")
                 .catch(e => console.log(e));
@@ -87,10 +100,9 @@ class Canvas extends React.Component {
     }
 
     render() {
-        return <div className="video-container">
-            <video id="video" className="video-container__video" ref={this.video}/>
-            <canvas id="canvas" className="background-canvas" ref={this.canvas}/>
-            <div className="image-container" ref={this.container} />
+        return <div className="canvas-container">
+            <video id="video" className="canvas-container__video" ref={this.video}/>
+            <canvas id="canvas" className="canvas-container__canvas" ref={this.canvas}/>
         </div>;
     }
 }
