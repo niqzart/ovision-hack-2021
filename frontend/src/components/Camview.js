e80d603cd8aa1bc7ce24a7db7388f40e82e0de5b
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-04-16 13:53:14 +0300

    added face real-time

--- frontend/src/components/Camview.js
+++ frontend/src/components/Camview.js
@@ -1,5 +1,20 @@
 import React from 'react';
-import {getFaceCoordinates, getTransformedImageFromVideo} from "../modules/face";
+import {getFaceCoordinates} from "../modules/face";
+
+
+async function getTransformedImageFromVideo(image_container, video, width, height) {
+    image_container.innerHTML = "";
+    let faceDimensions = await getFaceCoordinates(video);
+    for (let i = 0; i < faceDimensions.length; ++i) {
+        const iFace = faceDimensions[i];
+        let canvas = document.createElement("canvas");
+        canvas.setAttribute("width", width);
+        canvas.setAttribute("height", height);
+        let context = canvas.getContext("2d");
+        context.drawImage(video, iFace.topLeftX, iFace.topLeftY, iFace.width, iFace.height, 0, 0, width, height);
+        image_container.appendChild(canvas);
+    }
+}
 
 
 class Camview extends React.Component {
@@ -8,6 +23,10 @@ class Camview extends React.Component {
         this.video = React.createRef();
         this.container = React.createRef();
         this.canvas = React.createRef();
+        this.image = {
+            intervalMs: 600,
+            timeout: null
+        }
     }
 
 
@@ -22,9 +41,16 @@ class Camview extends React.Component {
             .catch(e => console.log(e));
     }
 
+    handleRepeatImageDisplay = () => {
+        this.image.timeout = setInterval(() => {
+            getTransformedImageFromVideo(this.container.current, this.video.current, 640, 640)
+                .catch(e => console.log(e));
+        }, this.image.intervalMs);
+    }
 
-    componentDidMount() {
+        componentDidMount() {
         // this.video.current.addEventListener('play', this.handleFaceCoordinates, false);
+
         const object = this;
         navigator.mediaDevices.getUserMedia({video: true, audio: false})
             .then((stream) => {
@@ -38,13 +64,14 @@ class Camview extends React.Component {
     }
 
     componentWillUnmount() {
-        //     this.video.current.removeEventListener('play', this.handleFaceCoordinates, false);
+        clearInterval(this.image.timeout);
+        // this.video.current.removeEventListener('play', this.handleFaceCoordinates, false);
     }
 
     render() {
         return <div className="camera">
             <button id="button__take" onClick={this.handleButtonClick} >Take photo</button>
-            <video id="video" height="480" width="640" onPlay={this.handleFaceCoordinates} ref={this.video}/>
+            <video id="video" height="480" width="640" onPlay={this.handleRepeatImageDisplay} ref={this.video}/>
             <div className="image-container" ref={this.container} />
             {/*<canvas id="canvas" height="480" width="640" ref={this.canvas}/>*/}
         </div>;
