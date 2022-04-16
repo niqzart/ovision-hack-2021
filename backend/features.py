8ddc796d375b8aed8f48b1415b5af56fac3dc114
Asya <asyaevloeva@gmail.com>
2022-04-16 04:57:44 +0300

    baseline

---
+++ backend/features.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+
+
+# The model architecture
+# download from: https://drive.google.com/open?id=1kiusFljZc9QfcIYdU2s7xrtWHTraHwmW
+AGE_MODEL = 'gender_net.caffemodel'
+# The model pre-trained weights
+# download from: https://drive.google.com/open?id=1kWv0AjxGSN0g31OeJa02eBGM0R_jcjIl
+AGE_PROTO = 'weights/age_net.caffemodel'
+# Each Caffe Model impose the shape of the input image also image preprocessing is required like mean
+# substraction to eliminate the effect of illunination changes
+MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+# Represent the 8 age classes of this CNN probability layer
+AGE_INTERVALS = ['(0, 2)', '(4, 6)', '(8, 12)', '(15, 20)',
+                 '(25, 32)', '(38, 43)', '(48, 53)', '(60, 100)']
+# download from: https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt
+FACE_PROTO = "weights/deploy.prototxt.txt"
+# download from: https://raw.githubusercontent.com/opencv/opencv_3rdparty/dnn_samples_face_detector_20180205_fp16/res10_300x300_ssd_iter_140000_fp16.caffemodel
+FACE_MODEL = "weights/res10_300x300_ssd_iter_140000_fp16.caffemodel"
+# Initialize frame size
+frame_width = 1280
+frame_height = 720
+# load face Caffe model
+face_net = cv2.dnn.readNetFromCaffe(FACE_PROTO, FACE_MODEL)
+# Load age prediction model
+age_net = cv2.dnn.readNetFromCaffe(AGE_MODEL, AGE_PROTO)
+
+#TODO: choose models and features to extract
