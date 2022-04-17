eb9a1a22295267399ad2a3b9ac99fcbfb0343239
qzart45hi <qwert45hi@yandex.ru>
2022-04-17 03:13:53 +0300

    fix: cleanup after rebase

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 def gen(camera):
     while True:
         frame, json_features = camera.get_frame()
@@ -16,10 +17,8 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
-if __name__ == '__main__':
-    app.run(debug=True)
