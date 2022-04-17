f868ad79b4d2733a6229a67ad635eabc7f9b5b6a
Asya <asyaevloeva@gmail.com>
2022-04-17 04:43:09 +0300

    landmarks added

--- backend/camera.py
+++ backend/camera.py
@@ -1,10 +1,12 @@
 import cv2
 from features import Features
+import time
+import dlib
 
 fm = Features()
 
 face_cascade=cv2.CascadeClassifier("models/haarcascades.xml")
-ds_factor=0.6
+ds_factor=0.7
 
 
 class Camera(object):
@@ -16,19 +18,50 @@ class Camera(object):
     
     def get_frame(self, ):
         success, image = self.video.read()
+        start = time.time()
         frameFace, bboxes = fm.getFaceBox(fm.faceNet, image)
-        json_features = self.get_features(bboxes, image, frameFace)
+        json_features = self.get_features(bboxes, image)
 
         image=cv2.resize(image,None,fx=ds_factor,fy=ds_factor,interpolation=cv2.INTER_AREA)
         gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-        face_rects=face_cascade.detectMultiScale(gray,1.3,5)
-        for (x,y,w,h) in face_rects:
+        face_rects=face_cascade.detectMultiScale(gray,1.1,50)
+        print('here!!')
+        # detector = dlib.get_frontal_face_detector()
+        # det = detector(gray, 1)
+        # rects = detector(gray, 1) #[[(348, 134) (669, 455)]]
+        # print('rectsss', rects)
+        # for (x,y,w,h) in face_rects:
+        for (i, rect) in enumerate(face_rects):
+            frameFace, bboxes = fm.getFaceBox(fm.faceNet, image)
+            json_features = self.get_features(bboxes, image)
+            print('rect', rect)
+            # print('det', det)
+            # print('i', i)
+
+            left = rect[0]
+            top = rect[1]
+            right = left+rect[2]
+            bottom = top+rect[3]
+            rect_landmarks = dlib.rectangle(left, top, right, bottom)
+
+            shape = fm.show_raw_detection(image, rect_landmarks, gray, i)
+            json_features.update({'landmarks':shape})
+            face_regions = fm.visualize_facial_landmarks(image, rect_landmarks, shape)
+            json_features.update(face_regions)
+            # print('face_regions', face_regions)
+            # fm.show_raw_detection(image, det[0], gray, i)
+            x, y, w, h = rect
+
             cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
             break
         ret, jpeg = cv2.imencode('.jpg', image)
+        end = time.time()
+        total = end - start
+        print('total time:', total)
+
         return jpeg.tobytes(), json_features
 
-    def get_features(self, bboxes, frame, frameFace):
+    def get_features(self, bboxes, frame):
 
         for bbox in bboxes:
             face = frame[max(0,bbox[1]-fm.padding):min(bbox[3]+fm.padding,frame.shape[0]-1),max(0,bbox[0]-fm.padding):min(bbox[2]+fm.padding, frame.shape[1]-1)]
@@ -49,6 +82,6 @@ class Camera(object):
             # label = "{},{}".format(gender, age)
             # cv2.putText(frameFace, label, (bbox[0], bbox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2, cv2.LINE_AA)
             # cv2.imshow("Age Gender Demo", frameFace)
-            return {'age':age, 'age_confidence':agePreds[0].max(), 'gender':gender, 'gender_confidence':genderPreds[0].max()}
+            return {'age':age, 'ageConfidence':agePreds[0].max(), 'gender':gender, 'genderConfidence':genderPreds[0].max()}
 
 
--- backend/features.py
+++ backend/features.py
@@ -1,11 +1,47 @@
 import numpy as np
+import dlib
 import cv2
+import os
+# from bz2 import decompress
+from urllib.request import urlretrieve
+from imutils import face_utils
+from collections import OrderedDict
+# from os.path import dirname, join
 
-from os.path import dirname, join
+
+# def download():
+#     # if type == 5:
+#     #     model_url = f"http://dlib.net/files/shape_predictor_5_face_landmarks.dat.bz2"
+#     #     model_name = "shape_predictor_5_face_landmarks.dat"
+#     # else:
+#     model_url = f"https://github.com/davisking/dlib-models/raw/master/shape_predictor_68_face_landmarks_GTX.dat.bz2"
+#     model_name = "shape_predictor_68_face_landmarks_GTX.dat"
+#
+#     if not os.path.exists(model_name):
+#         urlretrieve(model_url, model_name + ".bz2")
+#         with open(model_name, "wb") as new_file, open(model_name + ".bz2", "rb") as file:
+#             data = decompress(file.read())
+#             new_file.write(data)
+#         os.remove(model_name + ".bz2")
+#     return model_name
 
 
 class Features():
     def __init__(self):
+
+        # For dlib’s 68-point facial landmark detector:
+        self.FACIAL_LANDMARKS_68_IDXS = OrderedDict([
+            ("mouth", (48, 68)),
+            ("inner_mouth", (60, 68)),
+            ("right_eyebrow", (17, 22)),
+            ("left_eyebrow", (22, 27)),
+            ("right_eye", (36, 42)),
+            ("left_eye", (42, 48)),
+            ("nose", (27, 36)),
+            ("jaw", (0, 17))
+        ])
+
+
         faceProto = "models/opencv_face_detector.pbtxt"
         faceModel = "models/opencv_face_detector_uint8.pb"
 
@@ -23,8 +59,14 @@ class Features():
         self.ageNet = cv2.dnn.readNet(ageModel, ageProto)
         self.genderNet = cv2.dnn.readNet(genderModel, genderProto)
         self.faceNet = cv2.dnn.readNet(faceModel, faceProto)
+        # self.dlib_facelandmark = dlib.shape_predictor("shape_predictor_68_face_landmarks_GTX.dat")
 
         self.padding = 20
+        # self.padding = 0
+        # name = download()
+        # print(name)
+        self.landmarks = dlib.shape_predictor('shape_predictor_68_face_landmarks_GTX.dat')
+
 
 
     def getFaceBox(self, net, frame, conf_threshold=0.7):
@@ -47,3 +89,77 @@ class Features():
                 # cv2.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight / 150)), 8)
         return frameOpencvDnn, bboxes
 
+    def show_raw_detection(self, image, rect, gray, i):
+        # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # determine the facial landmarks for the face region, then
+        # convert the facial landmark (x, y)-coordinates to a NumPy
+        # array
+        shape = self.landmarks(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+        # print('shape:', shape)
+
+        # convert dlib's rectangle to a OpenCV-style bounding box
+        # [i.e., (x, y, w, h)], then draw the face bounding box
+        (x, y, w, h) = face_utils.rect_to_bb(rect)
+
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # show the face number
+        cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+        # loop over the (x, y)-coordinates for the facial landmarks
+        # and draw them on the image
+        for (x, y) in shape:
+            # cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # cv2.circle(image, (x - w, y - h), 1, (0, 0, 255), -1)
+            cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+        return shape
+
+
+    def visualize_facial_landmarks(self, img, landmarks, shape):
+        # create two copies of the input image -- one for the
+        # overlay and one for the final output image
+        # overlay = image.copy()
+        output = img.copy()
+
+        # if the colors list is None, initialize it with a unique
+        # color for each facial landmark region
+        # if colors is None:
+        colors = [(19, 199, 109), (79, 76, 240), (230, 159, 23),
+            (168, 100, 168), (158, 163, 32),
+            (163, 38, 32), (180, 42, 220), (0, 0, 255)]
+        #
+        # # loop over the facial landmark regions individually
+        for (i, name) in enumerate(self.FACIAL_LANDMARKS_68_IDXS.keys()):
+        #     # grab the (x, y)-coordinates associated with the
+        #     # face landmark
+            (j, k) = self.FACIAL_LANDMARKS_68_IDXS[name]
+            pts = shape[j:k]
+
+            # check if are supposed to draw the jawline
+            jaw = []
+            if name == "jaw":
+                # since the jawline is a non-enclosed facial region,
+                # just draw lines between the (x, y)-coordinates
+                for l in range(1, len(pts)):
+                    ptA = tuple(pts[l - 1])
+                    ptB = tuple(pts[l])
+                    cv2.line(img, ptA, ptB, colors[i], 2)
+                    # print('ptA, ptB, colors[i], 2', ptA, ptB, colors[i], 2)
+                    jaw.append([ptA, ptB])
+
+            # otherwise, compute the convex hull of the facial
+            # landmark coordinates points and display it
+            # else:
+            hull = cv2.convexHull(pts)
+            cv2.drawContours(img, [hull], -1, colors[i], -1)
+            # print('img, [hull], -1, colors[i], -1', img, [hull], -1, colors[i], -1)
+            face_regions = {'faceRegion':[hull], 'faceRegionColor':colors[i], 'jawLine':jaw}
+
+        # apply the transparent overlay
+        cv2.addWeighted(img, 0.70, output, 1 - 0.7, 0, output)
+
+        # return the output image
+        return face_regions
