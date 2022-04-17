8ea8827cc84be4ac751c10bf95fa3c99a488f005
qzart45hi <qwert45hi@yandex.ru>
2022-04-17 03:04:12 +0300

    feat: new socketio endpoints

--- backend/emitter.py
+++
@@ -1,5 +0,0 @@
-from flask_socketio import emit
-
-
-def send_metadata(sid, data: dict):
-    emit("metadata", data, room=sid)
--- backend/main.py
+++ backend/main.py
@@ -2,20 +2,34 @@ from flask import request
 from flask_socketio import SocketIO
 
 from app import app
-from emitter import send_metadata
+from validator import with_validation
 
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", logger=True, engineio_logger=True)
 
 
-@socketio.on("connect")
-def socket_connect(auth=None):
-    # send_metadata usage example
-    send_metadata(request.sid, {"hey": "world"})
+@socketio.event
+def connect(auth=None):
     print("connected:", auth, request.sid)
 
 
-@socketio.on("disconnect")
-def socket_disconnect():
+@socketio.event
+@with_validation(socketio)
+def stream(data):
+    face_id: int = data["face_id"]
+    image: str = data["image"]
+
+    response = {}  # temp, replace  # noqa
+    # do the machine learning here (pls)
+    # preferably in a function in a separate file
+    # create the response within that logic and return it to here
+
+    # response message
+    response["face_id"] = face_id
+    socketio.emit("metadata", response)
+
+
+@socketio.event
+def disconnect():
     print("disconnected:", request.sid)
 
 
